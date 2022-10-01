b7df1b96028affad91aaac0cb3c5a5a67a915d71
IannKM <78416691+IannKM@users.noreply.github.com>
2022-10-01 13:36:27 +0300

    new update

---
+++ associative_array.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <title> Strings </title>
+   <link rel="stylesheet" href="styles.css">
+</head>
+    <body>
+
+    <?php
+
+
+
+
+
+
+
+
+
+    ?>
+<br>
+<br>
+<a href='strings.php'><button class="btn0">BACK</button></a>
+<a href='#'><button class="btn1">NEXT</button></a>
+
+</body>
+</html>
