8162a6159925d7379426c962ede0d4f0cddc5fed
IannKM <78416691+IannKM@users.noreply.github.com>
2022-10-06 18:41:57 +0300

    new

--- 09dates.php
+++ 09dates.php
@@ -1,37 +1,43 @@
 <!doctype html>
 <html>
 <head>
-    <title> Functions </title>
+    <title> Dates</title>
    <link rel="stylesheet" href="styles.css">
 </head>
     <body>
 
     <?php
+// print current date
+echo date ('d-m/y H:i:s');
+echo ('<br>');
+
+// print yesterday
+echo date ('d-m/y H:i:s' ); echo ('<br>');
+echo date ('d-m/y H:i:s', time() - 60 * 68 *24 );echo ('<br>');
+
+// Different format: https://www.php.net/manual/en/datetime.format.php
+echo date('D, dS M-Y H:i:s'); echo ('<br>');
 
 
+// Print current timestamp
+echo time(); echo ('<br>');
 
 
+//parse date
 
+$parse = date_parse('27th-12-2021 09:30:58');
+echo '<pre>'; 
+print_r ($parse);
+echo'</pre>';
 
+// parse date fro format
 
+$date_string = 'January 5 2021 12:46:02';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$parsedate = date_parse_from_format('F j Y H:i:s', $date_string);
+echo '<pre>'; 
+print_r ($parsedate);
+echo'</pre>';
 
 
 
@@ -39,7 +45,7 @@
 <br>
 <br>
 <a href='08functions.php'><button class="btn0">BACK</button></a>
-<a href='09dates.php'><button class="btn1">NEXT</button></a>
+<a href='web.php'><button class="btn1">NEXT</button></a>
 
 </body>
 </html>
---
+++ 10including_files/website_skeleton.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+    <title> website </title>
+   <link rel="stylesheet" href="styles.css">
+</head>
+    <body >
+    <?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   ?>
+<br>
+<br>
+<a href='09dates.php'><button class="btn0">BACK</button></a>
+<a href='09dates.php'><button class="btn1">NEXT</button></a>
+
+</body>
+</html>
