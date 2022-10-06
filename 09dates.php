86c18b84b017d4384564e3d7aa5d3d4016c218d5
IannKM <78416691+IannKM@users.noreply.github.com>
2022-10-06 16:13:49 +0300

    dates

---
+++ 09dates.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+    <title> Functions </title>
+   <link rel="stylesheet" href="styles.css">
+</head>
+    <body>
+
+    <?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ?>
+<br>
+<br>
+<a href='08functions.php'><button class="btn0">BACK</button></a>
+<a href='09dates.php'><button class="btn1">NEXT</button></a>
+
+</body>
+</html>
