d4a6e2072d0842632b69ada90ec68e0a2bb50f7f
IannKM <78416691+IannKM@users.noreply.github.com>
2022-10-17 14:55:36 +0300

    OOP

---
+++ 12OOP/one.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="styles.css">
+<?php 
+
+
+
+
+
+
+
+?>
+<br>
+<br>
+<a href='../11FS/index.php'><button class="btn0">BACK</button></a>
+<a href="#"><button class="btn1">NEXT</button></a>
+<br></br>
