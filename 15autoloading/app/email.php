f0b76de9434d8eacfebd6754794b560c0c221a25
IannKM <78416691+IannKM@users.noreply.github.com>
2022-11-02 16:50:33 +0300

    autoloading

---
+++ 15autoloading/app/email.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace app;
+
+
+class Email 
+{
+    public function __construct()
+    {
+        echo "Email class".'<br>';
+    }
+
+
+
+
+}
+
+
+
+
+
+?>
---
+++ 15autoloading/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once "vendor/autoload.php";
+
+
+$email = new app\Email();
+$person = new app\Person();
+/* run composer init in this folder from terminal 
+leave everything as default */
+
+
+
+
+
+
+?>
